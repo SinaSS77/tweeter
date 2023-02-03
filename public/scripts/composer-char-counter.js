c83acb811d0696dddcdb19b9ef70f0f7fc67b826
SinaSS77 <s.molaverdikhani>
2023-02-02 18:32:31 -0800

    modified and completed

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,15 @@
 //responsible for character counter of tweet input form
 $(document).ready(function () {
+
+const escape = function (str){
+  let div = document
+}
+
+
+
   $('#tweet-text').on('keyup', function (e) {
-    //tracking character count input from user using keyup
+       //tracking character count input from user using keyup
+       
     const currentValue = $(this).val().length
     //locating the counter element
     const remainingChars = 140 - currentValue;
@@ -10,12 +18,12 @@ $(document).ready(function () {
     //updating counter elem on DOM to reflect current changes in remaining characters
     counterElement.text(remainingChars);
 
-    if (remainingChars < 0) {
+    if (remainingChars <= 0) {
       counterElement.css('color', 'red');
     } else if (remainingChars > 0 && remainingChars < 50){
       counterElement.css('color', 'orange');
+    } else{
+      counterElement.css('color', 'black');
     }
-      remainingChars.css('color', 'black');
-    
   })
 });
